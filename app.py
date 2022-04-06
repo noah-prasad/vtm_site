70f367cf7f702442f2031667e26784b6036cf26d
Noah Prasad <nprasad@uiowa.edu>
2022-04-05 23:06:10 -0500

    Added an estimates page, added an about  page, fixed issues with index.html, edited the sylesheet

--- app.py
+++ app.py
@@ -3,27 +3,39 @@ from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-friend_list = [{"name": "Mike Colbert", "email":"mike@mike.com" } ]
+price_estimate = [{"price": ""}]
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Mike\'s Friends', friends = friend_list)
+    return render_template('index.html', pageTitle='Vertical Tank Maintenance')
 
-@app.route('/mike')
-def mike():
-    return render_template('mike.html', pageTitle='About Mike')
+@app.route('/about')
+def about():
+    return render_template('about.html', pageTitle='About VTM')
 
-@app.route('/add_friend', methods=['GET', 'POST'])
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html', pageTitle='Price Estimator', estimates = price_estimate)
+
+@app.route('/estimate_price', methods=['GET', 'POST'])
 def add_friend():
     if request.method == 'POST':
         form = request.form
-        fname = form['fname']
-        lname = form['lname']
-        email = form['email']
-        friend_dict = {"name": fname + " " + lname, "email": email}
-        friend_list.append(friend_dict)
-        return redirect(url_for('index'))
-    return redirect(url_for('index'))
+        radius = int(form['radius'])
+        height = int(form['height'])
+        pi = 3.14
+        area_t = pi * (radius*radius)
+        area_s = 2*(pi*(radius * height))
+        tot_area = area_t + area_s
+        tot_sf = tot_area/144
+        tot_mc = tot_sf * 25
+        tot_lc = tot_sf * 15
+        total_cost = tot_mc + tot_lc
+        tot_cost = "$" + str(round(total_cost, 2))
+        cost_dict = {"price": tot_cost}
+        price_estimate.append(cost_dict)
+        return redirect(url_for('estimate'))
+    return redirect(url_for('estimate'))
     
 
 if __name__ == '__main__':
